c686ac74a06bcf425e6e06263229593889bb93f9
soobinhand <chelsea9528@naver.com>
2021-07-12 23:22:00 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import Landingpage from './pages/Landingpage'
-
+//js 파일 자체를 import 뒤에 있는 이름으로 가져온거임 그저.
+//중요한 건 어디서 뭘 가져오냐지, 저 별명 즉 이름은 중요하지 않아.
+//저 js 파일을 이런 이름으로 가져오겠다는 것임.
 
 //컴포넌트 만들어도 index.js는 현재 Landing만을 가지고 있어서 아무 영향이 없음.
 function App(){
