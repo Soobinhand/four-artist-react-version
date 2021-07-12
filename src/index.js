e109782a28e92446c62072c9c2350b5b05e3517a
soobinhand <chelsea9528@naver.com>
2021-07-12 23:03:46 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import A from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 //저기 밑에다가 컴포넌트 이름을 집어넣고 실행해야함.
 //그러려면 js 파일에서 컴퍼넌트를 임포트해야함
 ReactDOM.render(
   <React.StrictMode>
-    <A />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
