e99dd7ebbe83749f5c03bf00aa1042aa74f52894
soobinhand <chelsea9528@naver.com>
2021-07-14 15:41:35 +0900

    a

--- src/pages/Editmyprofile.js
+++ src/pages/Editmyprofile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../style/editmyprofile.css';
 import { Button, IconButton } from '@material-ui/core';
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
 class Editmyprofile extends Component{
     render(){
@@ -19,15 +20,16 @@ class Editmyprofile extends Component{
                         
                         <input type="text" placeholder="닉네임" className="nickname"></input>
                         <input type="text" placeholder="#해시태그 (최대 5개)" className="hashtag"></input>
-                        <input type="text" placeholder="간략소개" className="intro"></input>
                         <div>
                             <img src="img/인스타그램 로고 타원@3x.png" alt="" className="insta"></img>
                             <img src="img/Button(30)-small@3x.png" alt="" className="byun"></img>
                         </div>
                         <hr className="line"></hr>
 
-                       
-                        
+
+                        <textarea id="test" name="test" placeholder="간략소개 (최대 100자)" className="intro"></textarea>
+         
+
 
                     </div>
 
