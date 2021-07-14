5440c21945e0ef85c6002d5ffdf1ac52e0585d71
soobinhand <chelsea9528@naver.com>
2021-07-14 16:06:21 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Q from './pages/Editmyprofile'
+import Q from './pages/Landingpage'
 //js 파일 자체를 import 뒤에 있는 이름으로 가져온거임 그저.
 //중요한 건 어디서 뭘 가져오냐지, 저 별명 즉 이름은 중요하지 않아.
 //저 js 파일을 이런 이름으로 가져오겠다는 것임.
