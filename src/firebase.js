684018173e83260d34e43cf1582369cd54f22835
soobinhand <chelsea9528@naver.com>
2021-07-16 18:40:51 +0900

    a

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDiaGcKsPqudW7Gp831AGz61uVINW1lZvs",
-    authDomain: "facebook-a1ca7.firebaseapp.com",
-    databaseURL: "https://facebook-a1ca7.firebaseio.com",
-    projectId: "facebook-a1ca7",
-    storageBucket: "facebook-a1ca7.appspot.com",
-    messagingSenderId: "447821505436",
-    appId: "1:447821505436:web:081c1a5d4b37f065b51e59",
-    measurementId: "G-YTLKYG5M7F"
-  };
+  apiKey: "AIzaSyD7Cp2hJk6pp81Xu646k3jfY3bMZ16yFHE",
+  authDomain: "four-artist.firebaseapp.com",
+  databaseURL: "https://four-artist.firebaseio.com",
+  projectId: "four-artist",
+  storageBucket: "four-artist.appspot.com",
+  messagingSenderId: "290084440030",
+  appId: "1:290084440030:web:98b6a2f2c1515d708fbf4f",
+  measurementId: "G-T63KHF6EXC"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
