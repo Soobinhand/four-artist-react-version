561d33905b76e4d99d0c8964042cd5791f4f0b6e
soobinhand <chelsea9528@naver.com>
2021-07-24 02:37:25 +0900

    a

--- src/Notice.js
+++ src/Notice.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Notice.css';
+import Moment from 'react-moment'
+import NoticeList from './NoticeList';
 
 function Notice() {
+
+
     return (
         <div className='notice'>
-            여기는 알림입니다.
+
+            <div className="notice__moment">
+                <Moment format="MM월 DD일"></Moment>
+            </div>
+            <NoticeList className='notice__noticelist'
+            memimg=''
+            name='이름'
+            />
+            <NoticeList className='notice__noticelist'
+            memimg=''
+            name='이름'
+            />
+            <NoticeList className='notice__noticelist'
+            memimg=''
+            name='이름'
+            />
+
+            
         </div>
     )
 }
